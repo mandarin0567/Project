2adfbc62e5d7f2ada17f912d3e1c1e969c82f89b
mandarin0567 <weedtown0567@gmail.com>
2020-05-11 02:16:17 +0900

    게시판 일부 구현

--- serch_main/app/src/main/java/com/htc/serch_main/MainActivity.java
+++ serch_main/app/src/main/java/com/htc/serch_main/MainActivity.java
@@ -1,6 +1,8 @@
 package com.htc.serch_main;
 
 import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +10,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
 
 import java.util.Map;
 
@@ -56,6 +59,8 @@ public class MainActivity extends AppCompatActivity {
         Place=(TextView)findViewById(R.id.place);
         Place.setOnClickListener(places);
     }
+
+
 
     //병원 탭을 누를때 지도에 마커 찍히는 기능
     TextView.OnClickListener hosp = new View.OnClickListener() {
@@ -138,4 +143,5 @@ public class MainActivity extends AppCompatActivity {
             super.onBackPressed();
         }
     }
+
 }
