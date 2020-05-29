0968968b5bba83bc505a4e8c2bc465165f108c9d
이세원 <shoho4@hanmail.net>
2020-05-29 15:09:27 +0900

    로그인기능

---
+++ RegisterLogin/app/src/main/java/com/example/registerlogin/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.registerlogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tv_id, tv_pass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv_id = findViewById(R.id.et_id);
+        tv_pass = findViewById(R.id.tv_pass);
+
+
+        Intent intent = getIntent();
+        String userID = intent.getStringExtra("userID");
+        String userPass = intent.getStringExtra("userPass");
+
+        tv_id.setText(userID);
+        tv_pass.setText(userPass);
+
+
+    }
+}
